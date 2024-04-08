c8582c073a277ba279718265a37e1f3685929ce6
ZGA2519 <phuripat2519@gmail.com>
2024-04-09 04:26:25 +0700

    add lsp, greeter-ui, snip, and auto-cmp

--- init.lua
+++ init.lua
@@ -1,11 +1,7 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set rnu")
-vim.g.mapleader = " " 
-vim.g.maplocalleader = "\\"
+--Load additional config..
+require('vim-options')
 
+--Load Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,2 +1,14 @@
+--Set default nvim setting
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set rnu")
+vim.cmd("set nu")
+vim.g.mapleader = " " 
+vim.g.maplocalleader = "\\"
 
-
+--Set keymap for save and Exit
+vim.keymap.set('n', '<leader>qq', ':qa!<CR>')
+vim.keymap.set('n', '<leader>wq', ':wqa<CR>')
+vim.keymap.set('n', '<leader>w', ':w<CR>')
